87989c2b4eb3cc50d95c995b255f64da0dcf5060
admin <q597756870@163.com>
2016-06-26 22:25:36 +0800

    first commit

---
+++ Algorithms/BaseDataStruct/HTQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.BaseDataStruct
+{
+    class HTQueue<T>
+    {
+        class Node<T> {
+            public T data;
+            public Node<T> next;
+        }
+        Node<T> head = null;
+        Node<T> rear = null;
+        public void EnQueue(T t) { 
+            
+        }
+
+        public T DeQueue() {
+            if (head == null)
+                throw new Exception();
+            Node<T> temp = head;
+            head = head.next;
+            return temp.data;
+
+        }
+
+        
+    }
+}
---
+++ Algorithms/BaseDataStruct/HTStack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.BaseDataStruct
+{
+    class HTStack<T>
+    {
+        //the test cod of  stack
+        //HTStack<int> stack = new HTStack<int>();
+        //for (int i = 0; i < 11; i++) {
+        //    stack.Push(i);
+        //}
+        //stack.Pop();
+        //stack.Pop();
+        //Console.WriteLine(stack.GetTop());
+        //Console.WriteLine(stack.ToString());
+
+
+        public HTStack(){
+        
+        }
+        const int stacksize = 10;
+        int top = -1;
+        T[] stackarray = new T[stacksize];
+
+        public T Pop() {
+            if (top == -1) {
+                return default(T);
+            }
+            return stackarray[top--];
+        }
+
+        public void Push(T t) {
+            if (top == stackarray.Length - 1) {
+                T[] temp = stackarray;
+                stackarray = new T[stackarray.Length * 2];
+                Array.Copy(temp, stackarray, temp.Length);
+            }
+            stackarray[++top] = t;
+        }
+
+        public T GetTop() {
+            if (top == stackarray.Length - 1) {
+                return default(T);
+            }
+            return stackarray[top];
+        }
+
+        public override string ToString()
+        {
+            string temp = "";
+            for (int i = 0; i <=top; i++) {
+                temp += stackarray[i]+" ";
+            }
+            return temp+" size="+stackarray.Length.ToString();
+        }
+        public int GetTopIndex() {
+            return top;
+        }
+    }
+}
